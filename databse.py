eede7d0b76ac1afb7a118bf4564fcf7d7e68a69e
RomacH <romachgaming@gmail.com>
2024-10-26 11:53:50 +0300

    commit

--- databse.py
+++ databse.py
@@ -2,10 +2,23 @@ import sqlite3
 
 class DataBase:
     def __init__(self):
-        self.con = sqlite3.connect("games.db")
+        self.conn = sqlite3.connect("games.db")
+        self.conn.autocommit = True
+        self.cur = self.conn.cursor()
+
+
 
 def main():
-    DataBase()
+    DataBase().cur.execute("""
+CREATE TABLE team_history (
+    history_id INT AUTO_INCREMENT PRIMARY KEY,
+    team_id INT,
+    event_date DATE NOT NULL,
+    event_description TEXT,
+    FOREIGN KEY (team_id) REFERENCES teams(team_id) ON DELETE CASCADE
+);
+
+ """)
 
 if __name__ == "__main__":
     main()
