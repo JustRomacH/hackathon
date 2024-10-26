2257af06c48fe0d4c474208dbbf7451ccb1b1d99
RomacH <romachgaming@gmail.com>
2024-10-26 13:11:56 +0300

    commit

--- databse.py
+++ databse.py
@@ -6,19 +6,29 @@ class DataBase:
         self.conn.autocommit = True
         self.cur = self.conn.cursor()
 
+    # Возвращает выбранное значение
+    def get_value(self, value: str, table: str, cond: str, cond_value: int) -> int:
+        req = self.cur.execute(
+            f"""SELECT {value} FROM {table} WHERE {cond} = {cond_value}"""
+        )
+        return req.fetchone()[0]
 
+    # Возвращает выбранные значения всех юзеров
+    def get_values(self, value: str, table: str,  order: str = None, reverse: bool = False) -> list[list[int]]:
+        req = f"SELECT {value} FROM {table} "
+        # Порядок сортировки
+        if order:
+            req += f"ORDER BY {order} "
+            if reverse:
+                req += "DESC"
+        return self.cur.execute(req).fetchall()
+
+    # Изменяет выбранное значение
+    def update_value(self, table: str, value: str, new_value: str, cond_value: str) -> None:
+        self.cur.execute(f"""UPDATE {table} SET {value} = {new_value} WHERE {cond_value} = {value}""")
 
 def main():
-    DataBase().cur.execute("""
-CREATE TABLE team_history (
-    history_id INT AUTO_INCREMENT PRIMARY KEY,
-    team_id INT,
-    event_date DATE NOT NULL,
-    event_description TEXT,
-    FOREIGN KEY (team_id) REFERENCES teams(team_id) ON DELETE CASCADE
-);
-
- """)
+    DataBase()
 
 if __name__ == "__main__":
     main()
