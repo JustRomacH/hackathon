86852c4ac69e802163d704375912b2b7d360bdb8
RomacH <romachgaming@gmail.com>
2024-10-26 16:01:13 +0300

    commit

--- lapta/main/admin.py
+++ lapta/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Teams)
+admin.site.register(Participants)
+admin.site.register(Games)
+admin.site.register(Results)
+admin.site.register(Judges)
+admin.site.register(Scores)
+admin.site.register(Seasons)
+admin.site.register(Awards)
+admin.site.register(Matches)
+admin.site.register(Teamstatistics)
+admin.site.register(Teamhistory)
--- lapta/main/models.py
+++ lapta/main/models.py
@@ -1,3 +1,109 @@
 from django.db import models
 
-# Create your models here.
+class Awards(models.Model):
+    award_name = models.CharField(max_length=100)
+    season = models.ForeignKey('Seasons', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'awards'
+
+
+class Games(models.Model):
+    game_date = models.DateField()
+    location = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'games'
+
+
+class Judges(models.Model):
+    name = models.CharField(max_length=100)
+    experience_years = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'judges'
+
+
+class Matches(models.Model):
+    game = models.ForeignKey(Games, models.DO_NOTHING)
+    team1 = models.ForeignKey('Teams', models.DO_NOTHING)
+    team2 = models.ForeignKey('Teams', models.DO_NOTHING, related_name='mainmatch_team2_set')
+    winner_team = models.ForeignKey('Teams', models.DO_NOTHING, related_name='mainmatch_winner_team_set')
+
+    class Meta:
+        managed = False
+        db_table = 'matches'
+
+
+class Participants(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    team = models.ForeignKey('Teams', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'participants'
+
+
+class Results(models.Model):
+    score = models.IntegerField()
+    game = models.ForeignKey(Games, models.DO_NOTHING)
+    team = models.ForeignKey('Teams', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'results'
+
+
+class Scores(models.Model):
+    score = models.IntegerField()
+    game = models.ForeignKey(Games, models.DO_NOTHING)
+    judge = models.ForeignKey(Judges, models.DO_NOTHING)
+    team = models.ForeignKey('Teams', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'scores'
+
+
+class Seasons(models.Model):
+    year = models.IntegerField()
+    champion_team = models.ForeignKey('Teams', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'seasons'
+
+
+class Teams(models.Model):
+    team_name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    year_formed = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'teams'
+
+
+class Teamhistory(models.Model):
+    event_date = models.DateField()
+    event_description = models.TextField()
+    team = models.ForeignKey(Teams, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'team_history'
+
+
+class Teamstatistics(models.Model):
+    total_games = models.IntegerField()
+    total_wins = models.IntegerField()
+    total_losses = models.IntegerField()
+    team = models.ForeignKey(Teams, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'team_statistics'
