586ade680eb7b8f2242b8c400058badaf219c0ee
RomacH <romachgaming@gmail.com>
2024-10-27 12:31:02 +0300

    commit

--- lapta/main/views.py
+++ lapta/main/views.py
@@ -4,12 +4,12 @@ from .databse import DataBase
 
 # Create your views here.
 def home(request):
-    db = DataBase()
     return render(request, 'main/home.html')
 
 def teams(request):
     db = DataBase()
     teams_db = []
+
     for team in db.get_values("*", "teams"):
         team_id = team[0]
         stats_db = db.get_values(
@@ -19,6 +19,7 @@ def teams(request):
             "name, age", "participants", "team_id", team_id
         )
         teams_db.append(list(team) + list(stats_db[0]) + list(participants_db))
+
     return render(
         request, 'main/teams.html',
         {'teams': teams_db}
@@ -27,6 +28,7 @@ def teams(request):
 def matches(request):
     db = DataBase()
     matches_db = []
+
     for match in db.get_values("*", "matches"):
         game_id = match[1]
         match = list(match)
@@ -40,24 +42,39 @@ def matches(request):
         first_team = db.get_value("team_name", "teams", "id", scores_db[0][0])
         second_team = db.get_value("team_name", "teams", "id", scores_db[1][0])
         judges = []
+
         for team_score in scores_db:
             judges.append(
                 db.get_values(
                     "name, experience_years", "judges", "id", team_score[2])[0]
             )
+
         matches_db.append(list(match)
               + list(games_db)
               + [[first_team, scores_db[0][1], scores_db[0][2]]]
               + [[second_team, scores_db[1][1], scores_db[1][2]]]
               + list(judges)
         )
+
     return render(request, 'main/matches.html', {"matches": matches_db})
 
 def seasons(request):
     db = DataBase()
-
-    return render(request, 'main/seasons.html')
+    seasons_db = []
+    for season in db.get_values("*", "seasons"):
+        season_id = season[0]
+        year = season[1]
+        team_id = season[2]
+        team_info = db.get_values(
+            "team_name, city", "teams", "id", team_id
+        )[0]
+        team_name = team_info[0]
+        team_city = team_info[1]
+        seasons_db.append((season_id, year, team_name, team_city))
+    print(seasons_db)
+    return render(
+        request, 'main/seasons.html', {"seasons": seasons_db}
+    )
 
 def about(request):
-    db = DataBase()
     return render(request, 'main/about.html')
